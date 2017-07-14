2b2564c4fdbfaf1244870a5d29b05d2ea51eb087
ravi <ravi@dataraft.in>
2017-07-14 21:21:37 +0530

    added timeseries.py

--- scripts/timeseries.py
+++ scripts/timeseries.py
@@ -51,7 +51,7 @@ while True:
 						if sensor_config['variables'][vid]['type'] == 'number':
 							d = float(d)
 							key = devid+'_'+sid+'_'+vid
-							REDIS.ZADD(key, timestamp, d)
+							REDIS.zadd(key, timestamp, d)
 
 			message.delete()
 		except Exception, e:
