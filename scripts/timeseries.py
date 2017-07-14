b35a3b8ef19277fe8d2edf0cd013592ddfacd429
ravi <ravi@dataraft.in>
2017-07-14 20:47:01 +0530

    added timeseries.py

--- scripts/timeseries.py
+++ scripts/timeseries.py
@@ -17,6 +17,42 @@ REDIS = redis.Redis(host=HOST, port=PORT)
 sqs = boto3.resource('sqs')
 timeseries = sqs.get_queue_by_name(QueueName='timeseries')
 
-for message in timeseries.receive_messages(WaitTimeSeconds=10):
-	print message.body
-	message.delete()
+def getSensorConfig(devid):
+	sensor_config = REDIS.get(devid+'_sensor_config')
+	
+	if sensor_config:
+		sensor_config = json.loads(sensor_config)
+	else:
+		device = DEVICE.get_item(Key={'devid':devid})
+		if 'Item' in device:
+			sensor_config = device['Item'].get('sensor_config', None)
+			if sensor_config:
+				sensor_config = json.loads(sensor_config)
+			REDIS.set(devid+'_sensor_config', json.dumps(sensor_config))
+	return sensor_config
+
+
+while True:
+	for message in timeseries.receive_messages(WaitTimeSeconds=10):
+		try:
+			payload = json.loads(message.body)
+			devid = payload.get('devid', None)
+			sensor = int(payload.get('sensor', 0))
+			variables = payload.get('variables', [])
+			timestamp = payload.get('timestamp', 0)
+
+			sensor_config = getSensorConfig(devid)
+			sensor_config = sensor_config.get(sensor, None)
+			
+			if sensor_config and sensor_config['record']:
+				sid = sensor_config['sid']
+				for vid, d in enumerate(variables):
+				if d and sensor_config['variables'][vid]:
+					if sensor_config['variables'][vid]['type'] == 'number':
+						d = float(d)
+						key = devid+'_'+sid+'_'+vid
+						REDIS.ZADD(key, timestamp, d)
+
+			message.delete()
+		except Exception, e:
+			print 
