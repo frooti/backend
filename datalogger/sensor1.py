403197c841fc9bb97b8a82713d5e3b1b572e91e6
ravi <ravi@dataraft.in>
2017-07-16 13:24:07 +0530

    fixed sensor script

--- datalogger/sensor1.py
+++ datalogger/sensor1.py
@@ -9,8 +9,8 @@ from logging.handlers import RotatingFileHandler
 import traceback
 
 ## CONFIG ##
-SENSOR = 1
 SERIAL_PORT = '/dev/ttyUSB0'
+SENSOR = 1
 DEVID = None
 BAUDRATE = None
 REGEX = None
@@ -44,50 +44,52 @@ def closeConnections():
 ## SENSOR CONNECTION ##
 while True:
 	try:
-		with serial.Serial() as SER:
-			SER.port = SERIAL_PORT
-			SER.baudrate = BAUDRATE
-			SER.bytesize = serial.EIGHTBITS
-			SER.parity = serial.PARITY_NONE
-			SER.stopbits = serial.STOPBITS_ONE
-			SER.timeout = 5
+		if DEVID and BAUDRATE:
+			with serial.Serial() as SER:
+				SER.port = SERIAL_PORT
+				SER.baudrate = BAUDRATE
+				SER.bytesize = serial.EIGHTBITS
+				SER.parity = serial.PARITY_NONE
+				SER.stopbits = serial.STOPBITS_ONE
+				SER.timeout = 5
 
-			while not SER.isOpen():
-				try:
-					## SERIAL CONNECTION ##
-					SER.open()
-					logger.info('SERIAL CONNECTION OK.')
-					
-					## DATABASE CONNECTION ##
-					DB = sqlite3.connect('sensordata.db')
-					cursor = DB.cursor()
-					cursor.execute('CREATE TABLE IF NOT EXISTS sensor (sensor INT NOT NULL, timestamp INT NOT NULL, payload CHAR(500) NOT NULL);')
-					DB.commit()
+				while not SER.isOpen():
 					try:
+						## SERIAL CONNECTION ##
+						SER.open()
+						logger.info('SERIAL CONNECTION OK.')
+						
+						## DATABASE CONNECTION ##
+						DB = sqlite3.connect('sensordata.db')
 						cursor = DB.cursor()
-						cursor.execute('CREATE INDEX timestamp ON sensor (timestamp);')
+						cursor.execute('CREATE TABLE IF NOT EXISTS sensor (sensor INT NOT NULL, timestamp INT NOT NULL, payload CHAR(500) NOT NULL);')
 						DB.commit()
+						try:
+							cursor = DB.cursor()
+							cursor.execute('CREATE INDEX timestamp ON sensor (timestamp);')
+							DB.commit()
+						except:
+							pass
+						logger.info('DB CONNECTION OK.')
 					except:
-						pass
-					logger.info('DB CONNECTION OK.')
-				except:
-					time.sleep(1)
+						time.sleep(1)
 
-			while True: # read data
-				data = SER.readline()
-				if data:
-					rdata = [data]
-					if REGEX: # extract
-						match = re.search(r, data)
-						if match:
-							rdata = list(match.groups())
-							rdata = json.dumps(rdata)
+				while True: # read data
+					data = SER.readline()
+					if data:
+						rdata = [data]
+						if REGEX: # extract
+							match = re.search(r, data)
+							if match:
+								rdata = list(match.groups())
+								rdata = json.dumps(rdata)
 
-					logger.DEBUG(rdata)
-					cursor = DB.cursor()
-					cursor.execute("INSERT INTO sensor (sensor, timestamp, payload) VALUES ("+str(SENSOR)+", "+str(int(time.time()))+", '"+rdata+"')")
-					DB.commit()
+						logger.DEBUG(rdata)
+						cursor = DB.cursor()
+						cursor.execute("INSERT INTO sensor (sensor, timestamp, payload) VALUES ("+str(SENSOR)+", "+str(int(time.time()))+", '"+rdata+"')")
+						DB.commit()
 	except:
+		print traceback.print_exc()
 		logger.error(traceback.print_exc())
 		time.sleep(1)
 
