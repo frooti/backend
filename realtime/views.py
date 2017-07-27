e5fb765c6615786125e9939e14e11a161e64563d
ravi <ravi@dataraft.in>
2017-07-27 20:27:25 +0530

    updated device info.

--- realtime/views.py
+++ realtime/views.py
@@ -48,7 +48,7 @@ def getDeviceInfo(devices):
 
 			if q['Responses'].get('device', None):
 				for d in q['Responses']['device']:
-					if d and d.get('sensor_config', None).get('location', None):
+					if d and d.get('location', None):
 						d['sensor_config']['location']['properties'] = {}
 						d['sensor_config']['location']['properties']['name'] = 'karaikal'
 						d['sensor_config']['location']['properties']['dev_name'] = d['name']
