eade3dc52e7216e038f250065cb79ff31f7f7c69
ravi <ravi@dataraft.in>
2017-07-29 13:10:00 +0530

    fixing timeseries

--- scripts/timeseries.py
+++ scripts/timeseries.py
@@ -11,7 +11,7 @@ logger = logging.getLogger('timeseries')
 logger.propagate = False
 
 logging.getLogger('botocore').setLevel(logging.CRITICAL)
-logging.getLogger('boto3').setLevel(logging.CRITICAL))
+logging.getLogger('boto3').setLevel(logging.CRITICAL)
 
 # create a file handler
 handler = RotatingFileHandler('/var/log/backend/timeseries.log', maxBytes=10000000, backupCount=2)
